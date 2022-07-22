47c084d658ec8a0dfd0128d564bca02a6b203945
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-07-22 19:26:58 +1000

    added cards, hit and shuffle functionality

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { Fragment } from "react";
+import {Button, Card} from '@mui/material'
 function App() {
+  const [currDeck, setDeck] = React.useState({})
+  const [currCards, SetCards] = React.useState([])
+
+  function shuffle() {
+    fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then(res => res.json())
+    .then(data =>  setDeck(data))
+
+    SetCards([])
+  }
+
+  function getCard() {
+    fetch("https://www.deckofcardsapi.com/api/deck/"+ currDeck.deck_id +"/draw/?count=1")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      SetCards(prev =>[...prev, data.cards[0]])
+    })
+  }
+
+  let hand = currCards.map((item, i) => {
+    return (
+    <img key ={i} src= {item.image} style={{padding: '5px'}} />
+    )
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div style={{width: '100%', height:'100%', position:'fixed', backgroundColor:'green'}}>
+        <div style={{top: '60%', width: '100%',position: 'fixed',  display: 'flex', justifyContent: 'center'}}>
+          <div style ={{minWidth: '50%',maxWidth: '100%',  display: 'flex', flexDirection:'column'}}>
+            <Card sx={{backgroundColor:'darkgreen', display: 'flex', justifyContent:'center'}}>
+              {hand}
+            </Card>
+            <div style={{display: 'flex', justifyContent:'space-around', marginTop: '10px', width: '30%',  alignSelf:'center'}}>
+              <Button  variant="contained" onClick ={shuffle}>shuffle</Button>
+              <Button  variant="contained" onClick ={getCard}>Hit</Button>
+            </div>
+          </div>
+        </div>
+          
+      </div>
+    </React.Fragment> 
+  )
 }
 
 export default App;
