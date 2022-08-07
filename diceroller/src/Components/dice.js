266a274b5111acf9cf67afff2ded380931898869
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-08-07 23:33:10 +1000

    made the dice actually work

--- diceroller/src/Components/dice.js
+++ diceroller/src/Components/dice.js
@@ -1,35 +1,82 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { TextField, Button, Card } from "@mui/material";
+import { flexbox } from "@mui/system";
 
 
 
 export default function Dice() {
-    const [num, changeNum] = React.useState(1);
+    const [numOfDice, setNum] = React.useState(0)
+    const [dice, changeDice] = React.useState([]);
 
-    function setDiceNumber() {
-        changeNum(Math.floor(Math.random() * (7 - 1) ) + 1)
+    
+    function setDice(e) {
+        setNum(e.target.value)
     }
 
-    function Dices () {
-        return (
-            <div style=
-                    {{width: 50, height: 50, 
-                    backgroundColor: "yellow",  
-                    color: 'black', textAlign:'center',
-                    marginRight: '10px'
-                    }}
-                onClick={setDiceNumber}
-            >
-                        {num}
-            </div>
-        )
+    function placeDice() {
+        const num = Number(numOfDice)
+        if(Number.isInteger(num)) {
+            changeDice(() => {
+                const temp =  Array(num);
+                for(let i= 0; i < temp.length; i++) {
+                    temp[i] =1
+                }
+                return temp;
+            })  
+            console.log(dice)                 
+        }
     }
+    
+    let manyDice    
+    manyDice = dice.map((number, index) => {
+           return (
+            <div key={index} data-index={index}
+                    style=
+                        {{width: '50px', height:'50px', 
+                        backgroundColor: "yellow",  
+                        color: 'black', 
+                        margin: '10px',
+                        display:'flex', justifyContent:'center' 
+                    , alignItems:'center', padding:'10px'}}
+                        onClick={setDiceNumber}               
+                >
+                       {number}     
+                </div>
+    )})
+   
+    
+       
+    
+//Math.floor(Math.random() * (7 - 1) ) + 1
+    function setDiceNumber(e) {
+        const x = e.currentTarget.dataset.index
+        const copy = [...dice];
+        copy[x] = Math.floor(Math.random() * (7 - 1) ) + 1
+        changeDice(copy)
+        
+    }
+
+    function roll() {
+        const copy = Array(dice.length)
+        for(let i=0; i < copy.length; i++) {
+            copy[i] = Math.floor(Math.random() * (7 - 1) ) + 1
+        }
+        changeDice(copy)
+    }
+  
 
     return(
-        <div className = "diceHolder">
-           <Dices />
-           <Dices />
-           <Dices />
-        </div>
+       
+        <Card className="diceTable" sx={{backgroundColor:'red'}}>
+            <div style ={{width:'100%', display: 'flex', justifyContent:'center'}}>
+                <TextField id="outlined-basic" label="Number of Dice" variant="outlined"  sx={{backgroundColor:'white'}} onChange={setDice}/>
+                <Button variant="contained" onClick={placeDice}>Place Dice</Button>
+           </div>
+           <div style={{display:'flex', margin: '20px', flexWrap:'wrap'}}>
+             {manyDice}
+           </div>
+           <Button variant="contained" onClick={roll}>roll</Button>
+        </Card>
+        
     )
 }
